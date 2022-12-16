72ba9e1b5a2a5f7947d5c211175d4e60802fa5d7
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-12-16 13:15:04 -0600

    first commit

---
+++ DAL/ubicacionesDALL.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using BLL;
+
+namespace DAL
+{
+    public class ubicacionesDALL
+    {
+        SQLDBHelper cConexion;
+        public ubicacionesDALL() {
+            cConexion = new SQLDBHelper();
+        }
+        public bool Agregar(ubicaciones_BLL OubicacionesBLL)
+        {
+            SqlCommand cmdComando = new SqlCommand();
+
+            cmdComando.CommandText = "INSERT INTO Direcciones (Ubicacion, Latitud, Longitud) Values(@Ubicacion, @Latitud, @Longitud)";
+            cmdComando.Parameters.Add("@Ubicacion", SqlDbType.VarChar).Value = OubicacionesBLL.Ubicacion;
+            cmdComando.Parameters.Add("@Latitud", SqlDbType.VarChar).Value = OubicacionesBLL.Latitud;
+            cmdComando.Parameters.Add("@Longitud", SqlDbType.VarChar).Value = OubicacionesBLL.Longitud;
+
+            return cConexion.EjecutarComandoSQL(cmdComando);
+        }
+        public bool Eliminar(ubicaciones_BLL ubicaciones_BLL) 
+        {
+            SqlCommand cmdComando = new SqlCommand();
+            cmdComando.CommandText = "DELETE from Direcciones WHERE id = @id";
+            cmdComando.Parameters.Add("@id", SqlDbType.Int).Value = ubicaciones_BLL.ID;
+
+             return cConexion.EjecutarComandoSQL(cmdComando);
+        } 
+        public bool Modificar(ubicaciones_BLL OubicacionesBLL) 
+        {
+            SqlCommand cmdComando = new SqlCommand();
+            cmdComando.CommandText = "Update Direcciones SET Ubicacion=@Ubicacion, Latitud=@Latitud, Longitud=@Longitud WHERE id = @id";
+            cmdComando.Parameters.Add("@id", SqlDbType.Int).Value = OubicacionesBLL.ID;
+            cmdComando.Parameters.Add("@Ubicacion", SqlDbType.VarChar).Value = OubicacionesBLL.Ubicacion;
+            cmdComando.Parameters.Add("@Latitud", SqlDbType.VarChar).Value = OubicacionesBLL.Latitud;
+            cmdComando.Parameters.Add("@Longitud", SqlDbType.VarChar).Value = OubicacionesBLL.Longitud;
+
+            return cConexion.EjecutarComandoSQL(cmdComando);
+        }
+        public DataTable Listar() {
+            SqlCommand cmdComando = new SqlCommand();
+            cmdComando.CommandText = "SELECT * FROM Direcciones";
+            cmdComando.CommandType = CommandType.Text;
+            DataTable TablaResultante = cConexion.EjecutarSentenciaSQL(cmdComando);
+            return TablaResultante;
+
+
+        }
+    }
+}
